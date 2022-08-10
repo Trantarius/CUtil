f3d7d3af01d7fac9bd14511310764dab1fc89e0c
Trantarius <leborjt@yahoo.com>
2022-08-10 14:13:30 -0400

    impl timer

--- timer.hpp
+++ timer.hpp
@@ -1,1 +1,33 @@
- 
+#include <chrono>
+#include <string>
+
+#define CLOCK std::chrono::high_resolution_clock
+
+class Timer{
+    CLOCK::time_point start_time;
+public:
+    void start(){
+        start_time=CLOCK::now();
+    }
+    double stop(){
+        return std::chrono::duration_cast<std::chrono::duration<double>>(CLOCK::now()-start_time).count();
+    }
+
+    static std::string format(double t){
+        if(t<10.0/1000000000){
+            return "<10 ns";
+        }
+        if(t<10.0/1000000){
+            return std::to_string(int(t*1000000000))+" ns";
+        }
+        if(t<10.0/1000){
+            return std::to_string(int(t*1000000))+" us";
+        }
+        if(t<10.0/1){
+            return std::to_string(int(t*1000))+" ms";
+        }
+        return std::to_string(int(t))+" s";
+    }
+};
+
+#undef CLOCK
--- util.hpp
+++ util.hpp
@@ -1,1 +1,4 @@
 #include "timer.hpp"
+
+
+
