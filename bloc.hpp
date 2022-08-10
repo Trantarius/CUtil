baa3095ea2f2e719a0a1231010978ff058015e79
Trantarius <leborjt@yahoo.com>
2022-08-10 15:53:12 -0400

    switch from char to uchar

--- bloc.hpp
+++ bloc.hpp
@@ -1,21 +1,23 @@
 #pragma once
 #include <cstring>
 
+#define uchar unsigned char
+
 struct bloc{
-    char* ptr=NULL;
+    uchar* ptr=NULL;
     size_t size=0;
 
     bloc(){}
-    bloc(size_t size):ptr(new char[size]),size(size){}
-    bloc(char* ptr, size_t size):ptr(ptr),size(size){}
+    bloc(size_t size):ptr(new uchar[size]),size(size){}
+    bloc(uchar* ptr, size_t size):ptr(ptr),size(size){}
     bloc(const bloc& b):ptr(b.ptr),size(b.size){}
 
-    char* operator &(){return ptr;}
-    const char* operator &() const {return ptr;}
-    operator char*(){return ptr;}
-    operator const char*() const {return ptr;}
-    char& operator [] (long n){return (ptr)[(n%size+size)%size];}
-    const char& operator [] (long n) const {return (ptr)[(n%size+size)%size];}
+    uchar* operator &(){return ptr;}
+    const uchar* operator &() const {return ptr;}
+    operator uchar*(){return ptr;}
+    operator const uchar*() const {return ptr;}
+    uchar& operator [] (long n){return (ptr)[(n%size+size)%size];}
+    const uchar& operator [] (long n) const {return (ptr)[(n%size+size)%size];}
 
     static bloc copy(bloc& b){
         bloc ret(b.size);
@@ -23,7 +25,7 @@ struct bloc{
         return ret;
     }
     static const bloc copy(const bloc& b){
-        char* ptr=new char[b.size];
+        uchar* ptr=new uchar[b.size];
         memcpy(ptr,b,b.size);
         return bloc(ptr,b.size);
     }
