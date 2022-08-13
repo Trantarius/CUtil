4a6e6e80cc414a5709fbbc6c6212fd03f53c5023
Trantarius <leborjt@yahoo.com>
2022-08-13 15:05:29 -0400

    _

--- bloc.hpp
+++ bloc.hpp
@@ -1,9 +1,7 @@
 #pragma once
+#include "util.hpp"
 #include <cstring>
-#include <cinttypes>
 
-#define uchar unsigned char
-typedef uint64_t hash_t;
 
 struct bloc{
     uchar* ptr=NULL;
@@ -12,7 +10,7 @@ struct bloc{
     bloc(){}
     bloc(size_t size):ptr(new uchar[size]),size(size){}
     bloc(uchar* ptr, size_t size):ptr(ptr),size(size){}
-    bloc(const bloc& b):ptr(b.ptr),size(b.size){}
+    bloc(bloc& b):ptr(b.ptr),size(b.size){}
 
     uchar* operator &(){return ptr;}
     const uchar* operator &() const {return ptr;}
@@ -25,26 +23,23 @@ struct bloc{
         delete [] ptr;
     }
 
-    static bloc copy(bloc& b){
-        bloc ret(b.size);
-        memcpy(ret,b,b.size);
-        return ret;
+    void copy(const bloc& b){
+        memcpy(ptr,b.ptr,size>b.size?b.size:size);
     }
-    static const bloc copy(const bloc& b){
+
+    static bloc copy_of(const bloc& b){
         uchar* ptr=new uchar[b.size];
         memcpy(ptr,b,b.size);
         return bloc(ptr,b.size);
     }
 
-    hash_t hash() const{
-        hash_t h=0;
+    u64 hash() const{
+        u64 h=0;
         uchar off=0;
         for(size_t n=0;n<size;n++){
-            h^=((hash_t)ptr[n])<<off;
+            h^=((u64)ptr[n])<<off;
             off=(off+8)%57;
         }
         return h;
     }
 };
-
-#undef uchar
