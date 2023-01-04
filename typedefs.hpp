ec9552ae07be6c26168725661ccbf2334c4c71fc
Trantarius <leborjt@yahoo.com>
2023-01-04 17:47:50 -0500

    use std::min/max

--- typedefs.hpp
+++ typedefs.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <cinttypes>
+#include <cstdlib>
+#include <algorithm>
 
 typedef int8_t i8;      typedef uint8_t u8;
 typedef int16_t i16;    typedef uint16_t u16;
@@ -7,3 +9,6 @@ typedef int32_t i32;    typedef uint32_t u32;
 typedef int64_t i64;    typedef uint64_t u64;
 
 typedef unsigned char uchar;
+
+using std::min;
+using std::max;
