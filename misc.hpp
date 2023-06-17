cb76002012d9428c6a7c1b3b42fa246b671f763a
Trantarius <leborjt@yahoo.com>
2023-06-17 17:20:26 -0400

    fix bug with large bitrolls

--- misc.hpp
+++ misc.hpp
@@ -1,7 +1,7 @@
 #include <cinttypes>
 
 inline uint64_t bitroll(uint64_t num,int8_t by){
-  return (num<<by)|(num>>(64-by));
+  return (num<<(by%64))|(num>>((64-by)%64));
 }
 
 inline int64_t bitroll(int64_t num,int8_t by){
@@ -9,7 +9,7 @@ inline int64_t bitroll(int64_t num,int8_t by){
 }
 
 inline uint32_t bitroll(uint32_t num,int8_t by){
-  return (num<<by)|(num>>(32-by));
+  return (num<<(by%32))|(num>>((32-by)%32));
 }
 
 inline int32_t bitroll(int32_t num,int8_t by){
@@ -17,7 +17,7 @@ inline int32_t bitroll(int32_t num,int8_t by){
 }
 
 inline uint16_t bitroll(uint16_t num,int8_t by){
-  return (num<<by)|(num>>(16-by));
+  return (num<<(by%16))|(num>>((16-by)%16));
 }
 
 inline int16_t bitroll(int16_t num,int8_t by){
@@ -25,7 +25,7 @@ inline int16_t bitroll(int16_t num,int8_t by){
 }
 
 inline uint8_t bitroll(uint8_t num,int8_t by){
-  return (num<<by)|(num>>(8-by));
+  return (num<<(by%8))|(num>>((8-by)%8));
 }
 
 inline int8_t bitroll(int8_t num,int8_t by){
